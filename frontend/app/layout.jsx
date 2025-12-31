70d8eccd33cdf9a61e1e13234de0ed5e2faf9458
Kaushal Neupane <kaushal98211@gmail.com>
2025-12-31 12:30:47 +0545

    json +ld added with schema.org in layout and robots and sitemap were added

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -20,14 +20,52 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `{
+        "@context": "https://schema.org",
+        "@type": ["Hotel", "Resort", "LodgingBusiness"],
+        "@id": "https://nvr-pi.vercel.app/#hotel",
+        "name": "Narayani View Resort",
+        "alternateName": "Narayani View Resort Nepal",
+        "description": "Narayani View Resort is a premium riverside resort in Nepal offering comfortable rooms, scenic Narayani river views, authentic local cuisine, and warm hospitality.",
+        "url": "https://nvr-pi.vercel.app/",
+        "logo": "https://nvr-pi.vercel.app/images/logo/logo.png",
+        "image": [
+          "https://nvr-pi.vercel.app/images/about/narayaniscene.png",
+          "https://nvr-pi.vercel.app/images/rooms/heroroom.jpg",
+          "https://nvr-pi.vercel.app/images/rooms/room1.jpg"
+        ],
+        "telephone": "+9779845974748",
+        "priceRange": "$$",
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "Narayani Riverside",
+          "addressLocality": "Golaghat, Meghauli",
+          "addressRegion": "Bagmati Province",
+          "postalCode": "44200",
+          "addressCountry": "NP"
+        },
+        "geo": {
+          "@type": "GeoCoordinates",
+          "latitude": 27.598231019429466,
+          "longitude": 84.16782884047078
+        },
+        "checkinTime": "14:00",
+        "checkoutTime": "12:00"
+      }`,
+          }}
+        />
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
-       <main>                                                                                                           
-         {children}
-       </main>
-        <Footer/>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
