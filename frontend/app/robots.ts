d2a3e7605f4972eced20cc8963f80900f1cdd7a2
Kaushal Neupane <kaushal98211@gmail.com>
2025-12-31 12:46:57 +0545

    changed relative path in robot.txt to absolute path

--- frontend/app/robots.ts
+++ frontend/app/robots.ts
@@ -1,19 +1,19 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = "nvr-pi.vercel.app";
+    const baseUrl = "https://nvr-pi.vercel.app";
     return {
         rules: [
             {
                 userAgent: "*",
                 allow: "/",
-                disallow: ["/contact/", "/api/"],
+                // disallow: ["/contact/", "/api/"],
             },
-            {
-                userAgent: "Googlebot",
-                allow: "/",
-                disallow: ["/terms-and-conditions/"],
-            }
+            // {
+            //     userAgent: "Googlebot",
+            //     allow: "/",
+            //     disallow: ["/terms-and-conditions/"],
+            // }
         ],
         sitemap: `${baseUrl}/sitemap.xml`,
     }
