7ea9554354384ac7c390e50b224edcbc4d22197b
Kaushal Neupane <kaushal98211@gmail.com>
2025-12-31 15:17:54 +0545

    prevent security vulnarities

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,12 +2,36 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-     remotePatterns: [
+    remotePatterns: [
       {
         protocol: "https",
         hostname: "images.pixabay.com",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' https://maps.googleapis.com 'unsafe-inline' 'unsafe-eval';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https://maps.googleapis.com https://maps.gstatic.com;
+              frame-src https://www.google.com;
+              connect-src 'self' https://maps.googleapis.com;
+            `.replace(/\s{2,}/g, " "),
+          },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "no-referrer-when-downgrade" },
+          { key: "Cross-Origin-Resource-Policy", value: "same-origin" },
+        ],
+      },
+    ];
   },
 };
 
